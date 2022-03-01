eecc0307333233a3c09930098e723cb4146c0262
sporkupine <coffeechief25@gmail.com>
2022-02-28 19:40:31 -0600

    initialize repository

---
+++ lib/ruby/cli/exercise.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "exercise/version"
+
+module Ruby
+  module Cli
+    module Exercise
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
